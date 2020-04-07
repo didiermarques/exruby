0171e5fc072ac0fa89ecb6c3164c13333d07f023
Detsou <detsouvan@hotmail.com>
2020-04-07 10:00:35 +0200

    ajout du file 02

---
+++ exo_02.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, monde !", "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# puts, décompose chaque arrays, et met un retour à la ligne à la fin, print imprime vraiment chaque élément tel quel.

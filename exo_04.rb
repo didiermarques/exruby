3137b49dbb31c52e5f46188f0aaacfde6ffa3a4b
Detsou <detsouvan@hotmail.com>
2020-04-07 10:10:25 +0200

    exo4 et exo5

---
+++ exo_04.rb
@@ -0,0 +1,12 @@
+=begin le programme ne se lance pas avec ce message d'erreur :
+
+exo_04.rb:1: unterminated string meets end of file
+puts "Salut, ça farte ?
+                        ^
+
+Parce que la string n'a pas été fermé avec le ", à l'endroit montré ci-dessus.
+Il aurait fallu écrire: puts "Salut, ça farte ?"
+=end
+
+puts "Salut, ça farte ?
+

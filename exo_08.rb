788349b5fc8c54b46c9832dd93fec7b1558bbe3d
Detsou <detsouvan@hotmail.com>
2020-04-07 10:56:43 +0200

    exo 8 et 9

---
+++ exo_08.rb
@@ -0,0 +1,6 @@
+puts "Salut toi, c'est quoi ton p'tit nom ?"
+
+print "Mon p'tit nom c'est "
+user_name = gets.chomp
+
+puts "Bonjour mon p'tit #{user_name} !"
---
+++ exo_09.rb
@@ -0,0 +1,8 @@
+puts "Salut toi, c'est quoi ton p'tit nom ?"
+
+print "Mon p'tit nom c'est "
+user_name = gets.chomp
+print "Et mon prénom c'est "
+user_firstname = gets.chomp
+
+puts "\nBonjour #{user_firstname} #{user_name}!"
